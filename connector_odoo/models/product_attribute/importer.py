5fddf47973bf1d9f54da233a7c7cf001ebb0395c
yibudak <yigit@altinkaya.com>
2025-02-20 12:00:36 +0000

    [REM] Removed old way to control record uniqueness
    Signed-off-by: yibudak <yigit@altinkaya.com>

--- connector_odoo/models/product_attribute/importer.py
+++ connector_odoo/models/product_attribute/importer.py
@@ -39,18 +39,6 @@ class ProductAttributeMapper(Component):
         ("visibility", "visibility"),
     ]
 
-    @only_create
-    @mapping
-    def check_att_exists(self, record):
-        domain = [("name", "=", record["name"])]
-        if create_variant := record.get("create_variant"):
-            domain.append(("create_variant", "=", create_variant))
-        att_id = self.env["product.attribute"].search(domain, limit=1)
-        res = {}
-        if att_id:
-            res.update({"odoo_id": att_id.id})
-        return res
-
     @mapping
     def create_variant(self, record):
         res = {"create_variant": "no_variant"}
