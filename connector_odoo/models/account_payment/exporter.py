315851ee8c96147c78d4329bd02099ba59a5017f
Ahmet Yigit Budak <yigit@altinkaya.com>
2025-04-02 00:37:08 +0300

    Add date mapping to AccountPaymentExportMapper
    Signed-off-by: Ahmet Yigit Budak <yigit@altinkaya.com>

--- connector_odoo/models/account_payment/exporter.py
+++ connector_odoo/models/account_payment/exporter.py
@@ -28,7 +28,6 @@ class AccountPaymentExportMapper(Component):
         ("ref", "ref"),
         ("payment_type", "payment_type"),
         ("partner_type", "partner_type"),
-        ("date", "date"),
     ]
 
     @mapping
@@ -61,6 +60,12 @@ class AccountPaymentExportMapper(Component):
         binder = self.binder_for("odoo.res.currency")
         return {
             "currency_id": binder.to_external(record.currency_id, wrap=True),
+        }
+
+    @mapping
+    def date(self, record):
+        return {
+            "date": record.date.strftime(DEFAULT_SERVER_DATETIME_FORMAT),
         }
 
     @only_create
